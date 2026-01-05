6d77d89fa28b9671839ebd3905b6d3d0d7a26090
viktor barath <viktor.barath7@gmail.com>
2026-01-05 19:18:27 +0100

    Add new pages; update style and html

---
+++ public/script.js
@@ -0,0 +1,17 @@
+
+
+// functionality for registration and login
+
+/* function register() {
+    fetch("/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({
+            username: regUsername.value,
+            password: regPassword.value
+        })
+    })
+    .then(res => res.json())
+    .then(data => MessageChannel.innerTest = data.message);
+}
+ */
